bc944f039f98ec21fffe33937c9d30c10ec47404
enaymerich <enricoaymerich@gmail.com>
2021-10-08 16:22:21 +0200

    changed gitignore

--- examples/diffusion_2d_weakBC.py
+++ examples/diffusion_2d_weakBC.py
@@ -17,32 +17,34 @@ torch.backends.cudnn.enabled= False
 torch.backends.cudnn.deterministic=True
 torch.backends.cudnn.benchmark = False
 # size of the tile
-height = 1
-width = 1
+height = 1#5e-3
+width = 1#28e-3
 mu = width/2
 sigma = 1e-1
-C = 1#dde.Variable(2.0)
-decay_steps = 1e3
-decay_rate = 1e-4
-Time_size = 1#height*width/C
+alpha = 1
+#decay_steps = 1e3
+#decay_rate = 1e-4
+Time_size = 0.1#height*width/C
 
 ## Domain par
-ND = 10000
-Nx = int(ND**(1/4))
-Ny = Nx
-Nt = Nx*Ny
-Nsx = Nx*Nt
-Nsy = Ny*Nt
-Nst = Nx*Ny*2
+ND = 10000# Nx*Ny*Nt
+Nx = int((ND/alpha*height/width)**(1/4))#20#
+Ny = int(Nx*width/height)
+Nt = int(Nx*Ny*alpha*Time_size/width/height)
+Nsx = Ny*Nt#1000
+Nsy = Nx*Nt#2000
+Nst = Nx*Ny#100
+
 num_test = 5000
 ## Network parameters
-lr = 5e-4
+lr = 3e-4
 loss = 'MSE'
 optim = "adam"
 ## weights
-weights = [1,10,1,1]
-weights = np.ndarray.tolist(np.exp(weights)/sum(np.exp(weights)))
-weights = [0, 0, 0, 1]
+weights = [0.25,0.75,0.75,0.75]
+#weights = np.ndarray.tolist(np.exp(weights)/sum(np.exp(weights)))
+#weights = [0, 0, 0, 1]
+logname = 'theo_deepxde_BC_weak_alpha075'
 
 
 def pde(x, y):
@@ -50,12 +52,11 @@ def pde(x, y):
     dy_xx = dde.grad.hessian(y, x, i=0, j=0)
     dy_yy = dde.grad.hessian(y, x, i=1, j=1)
     # Backend pytorch
-    return dy_t - C * (dy_xx+dy_yy)#((dy_t.squeeze() - C * (dy_xx+dy_yy).squeeze())* torch.exp(-x[:,2])).unsqueeze(1)
+    return dy_t - alpha * (dy_xx+dy_yy)#((dy_t.squeeze() - C * (dy_xx+dy_yy).squeeze())* torch.exp(-x[:,2])).unsqueeze(1)
 
 
 def boundary_x(x, on_boundary):
-    return on_boundary & np.isclose(x[0], 0)
-
+    return on_boundary & np.isclose(x[0], 0)#qui xlim
 
 def boundary_y(x, on_boundary):
     return (on_boundary & np.isclose(x[1], 0)) or (on_boundary & np.isclose(x[1], width))
@@ -70,7 +71,7 @@ def func_y(x):
 
 
 def func_IC(x):
-    return x[:, 1:2]*0
+    return np.exp(-(mu-x[:, 1:2])**2/(2*sigma**2))*np.exp(-(x[:, 0:1])**2/(2*sigma**2))
 
 
 def show_train_points(data):
@@ -85,7 +86,7 @@ def show_train_points(data):
     fig2 = plt.figure()
     ax2 = fig2.add_subplot(111, projection='3d')
     bc_vals = [func_x(bc_points[i:i+1,:]) if bc_points[i,0]==0 else 0 for i in range(len(bc_points))]
-    SC2 = ax2.scatter(bc_points[:,2], bc_points[:,0], bc_points[:,1],c=bc_vals)
+    SC2 = ax2.scatter(bc_points[:,2], bc_points[:,0], bc_points[:,1], c=bc_vals)
     ax2.set_xlabel('t')
     ax2.set_ylabel('x')
     ax2.set_zlabel('y')
@@ -100,27 +101,28 @@ timedomain = dde.geometry.TimeDomain(0, Time_size)
 geomtime = dde.geometry.GeometryXTime(geom, timedomain)
 
 #bc1 = dde.DirichletBC(geomtime, func, lambda _, on_boundary: )
-bc_x = dde.DirichletBC(geomtime, func_x, boundary_x, component=0, num_points=200)
-bc_y = dde.NeumannBC(geomtime, func_y, boundary_y, component=0, num_points=Nsy*2)
+bc_x = dde.DirichletBC(geomtime, func_x, boundary_x, component=0, num_points=Nsx)#Nsx)
+bc_y = dde.NeumannBC(geomtime, func_y, boundary_y, component=0, num_points=Nsy*2)#)
 
 #)
 ic = dde.IC(geomtime, func_IC, lambda _, on_initial: on_initial)
 
-
+#np.array(np.array([[0,0,0],[0,0,1],[0,1,0],[0,1,1]],[1,0,0],[1,0,1],[1,1,0],[1,1,1]]))
 data = dde.data.TimePDE(
     geomtime,
     pde,
     [bc_x, bc_y, ic],
     num_domain=ND,  #ND
-    num_boundary=int((Nsx+Nsy*2)*1.5), #Nx+Ny
+    num_boundary=(Nsx+Nsy)*2, #Nx+Ny
     num_initial=Nst,  #Nt
     num_test=num_test,
     train_distribution="pseudo",
     seed=seed,
+ #   exclusions=
 )
 #show_train_points(data)
 
-layer_size = [3] + [50] * 1 + [1]
+layer_size = [3] + [32] * 2 + [1]
 activation = "tanh"
 initializer = "Glorot uniform"
 net = dde.maps.FNN(layer_size, activation, initializer)
@@ -130,11 +132,18 @@ net = dde.maps.FNN(layer_size, activation, initializer)
 #    initializer_zero(net.linears[-1].bias)
 if net.linears[-1].bias is not None:
      nn.init.constant_(net.linears[-1].bias.data, 0.5)
-model = dde.Model(data, net)
 
+
+model = dde.Model(data, net)
+loss_names=['PDE_loss', 'BC_x loss','BC_y loss','IC loss']
 model.compile(optim, lr=lr, loss=loss, loss_weights=weights)
-resampler = dde.callbacks.PDEResidualResampler(period=5000)
-losshistory, train_state = model.train(epochs=100000, display_every=250,
-                                       model_save_path='theo_deepxde.pt', callbacks=[resampler])
+resampler = dde.callbacks.PDEResidualResampler(period=10000)
+losshistory, train_state = model.train(epochs=500000, display_every=1000, save_every=5000,
+                                       model_save_path=logname+'.pt',
+                                       #model_restore_path=logname+'.pt',
+                                       logname=logname,
+                                       callbacks=[resampler],
+                                       Tensorboard=True, loss_names=loss_names)
 
 dde.saveplot(losshistory, train_state, issave=True, isplot=True)
+dde.save_loss_history(losshistory,logname+'.txt')
